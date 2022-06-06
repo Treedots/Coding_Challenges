c1a05fe87115a6777f5c4c8af301db3761feb535
Treedots <Treedots@users.noreply.github.com>
2022-06-06 14:20:14 +0800

    Delete Algorithm/Warmup directory

--- Algorithm/Warmup/Diagonal Difference.cpp
+++
@@ -1,9 +0,0 @@
-int diagonalDifference(vector<vector<int>> arr) {
-    int result_a = 0;
-    int result_b = 0;
-    for(int i=0;i<arr.size();i++){
-        result_a += arr[i][i];
-        result_b += arr[arr.size()-1-i][i];
-    }
-    return abs(result_b-result_a);
-}
