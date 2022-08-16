3ec816c6d88f99902db1e8b3c7354d575e342a42
Treedots <Treedots@users.noreply.github.com>
2022-08-16 15:22:49 +0800

    Create Valid Parentheses.js

---
+++ codeWar/javaScript/Valid Parentheses.js
@@ -0,0 +1,22 @@
+function validParentheses(parens) {
+  // your code here ..
+  let len = parens.length;
+  if( len % 2 == 1)
+      return false;
+  else{
+    let queue = [];
+    for(i=0;i<len;i++){
+      let t = parens[i];
+      if(queue.length == 0 && t == ")"){
+        return false
+      }
+      else if(queue[queue.length-1]=="(" && t == ")"){
+        queue.shift();
+      }
+      else{
+        queue.push(parens[i]);
+      }
+    }
+    return queue.length>0?false:true;
+  }
+}
